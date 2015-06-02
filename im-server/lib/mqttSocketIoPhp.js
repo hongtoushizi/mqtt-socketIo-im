87a0b540657195ddcb7fe39511bc487447633fe9
yuan <yuanhongshuai3.11@163.com>
2015-06-02 12:11:14 +0800

    test

--- im-server/lib/mqttSocketIoPhp.js
+++ im-server/lib/mqttSocketIoPhp.js
@@ -5,37 +5,37 @@ global.mqtt = require('mqtt');
 
 //由于mysql版本问题，暂时屏蔽。
 
- var mysql = require('mysql');
- var db_config = {
- host: 'localhost',
- user: 'kuulabu',
- password: 'kuulabu',
- database: 'Kuulabu'
- };
-
- var connection;
-
- function handleDisconnect() {
- connection = mysql.createConnection(db_config); // Recreate the connection, since
- // the old one cannot be reused.
- connection.connect(function(err) {              // The server is either down
- if(err) {                                     // or restarting (takes a while sometimes).
- console.log('error when connecting to db:', err);
- setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
- }                                     // to avoid a hot loop, and to allow our node script to
- });                                     // process asynchronous requests in the meantime.
- // If you're also serving http, display a 503 error.
- connection.on('error', function(err) {
- console.log('db error', err);
- if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
- handleDisconnect();                         // lost due to either server restart, or a
- } else {                                      // connnection idle timeout (the wait_timeout
- throw err;                                  // server variable configures this)
- }
- });
- }
-
- handleDisconnect();
+ //var mysql = require('mysql');
+ //var db_config = {
+ //host: 'localhost',
+ //user: 'kuulabu',
+ //password: 'kuulabu',
+ //database: 'Kuulabu'
+ //};
+ //
+ //var connection;
+ //
+ //function handleDisconnect() {
+ //connection = mysql.createConnection(db_config); // Recreate the connection, since
+ //// the old one cannot be reused.
+ //connection.connect(function(err) {              // The server is either down
+ //if(err) {                                     // or restarting (takes a while sometimes).
+ //console.log('error when connecting to db:', err);
+ //setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+ //}                                     // to avoid a hot loop, and to allow our node script to
+ //});                                     // process asynchronous requests in the meantime.
+ //// If you're also serving http, display a 503 error.
+ //connection.on('error', function(err) {
+ //console.log('db error', err);
+ //if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+ //handleDisconnect();                         // lost due to either server restart, or a
+ //} else {                                      // connnection idle timeout (the wait_timeout
+ //throw err;                                  // server variable configures this)
+ //}
+ //});
+ //}
+ //
+ //handleDisconnect();
  
 Array.prototype.indexOf = function (val) {
 	for (var i = 0; i < this.length; i++) {
